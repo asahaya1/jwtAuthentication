3c3833c422ba58aaf4fbb939be53e06eaadd7d52
asahaya1 <hayato_asami@narutech.co.jp>
2023-06-23 16:26:50 +0900

    commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
-
 const jwt = require('jsonwebtoken')
 
 app.use(express.json())
@@ -20,22 +21,14 @@ app.get('/posts', authenticateToken, (req, res) => {
   res.json(posts.filter(post => post.username === req.user.name))  
 })
 
-app.post('/login',(req, res) => {
-  //Authenticate User
-
-  const username = req.body.username
-  const user = { name:username}
-
-  const accessToken = jwt.sign(user, process.env.ACCECC_TOKEN_SECRET)
-  res.json({ accessToken: accessToken})
-})
 
 function authenticateToken(req, res, next){
   const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(' ')[1]
   if (token == null) return res.sendStatus(401)
 
-  jwt.verify(token, process.env.ACCECC_TOKEN_SECRET,(err, user) => {
+  jwt.verify(token, process.env.ACCECC_TOKEN_SECRET, (err, user) => {
+    console.log(err)
     if(err) return res.sendStatus(403)//アクセス権がないことを表示
     req.user = user
     next()
