ac9dc33129915af89e585212f621c2f949415692
asahaya1 <hayato_asami@narutech.co.jp>
2023-07-03 10:16:31 +0900

    commit

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.post('/login', (req, res) => {
   const username = req.body.username
   const user = { name: username };
 
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  const accessToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
   res.json({ accessToken: accessToken });
 
 })
